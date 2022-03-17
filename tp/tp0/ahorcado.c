dfafe3442a62d935ddad876259682d532dc764c4
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2022-03-17 10:55:10 -0300

    tp0

---
+++ tp/tp0/ahorcado.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>	/* para las funciones system y exit */
+
+int main() {
+
+	int c;
+
+	/* Decirle al sistema que el modo input es RAW */
+	system ("/bin/stty raw");
+
+	while(1) {
+		printf("\r                                                          ");
+		printf("\r c = %c  ingrese una letra (0 para salir): ", c);
+		c = getchar();
+
+		if (c == '0')
+			exit(0);
+	}
+
+}
